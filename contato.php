99e9d2138d0d0885e6269e7cf37e084b4e16baed
MarianeCamilo <marianecamilo03@gmail.com>
2023-08-11 21:58:36 -0300

    Entrega 2

--- contato.php
+++ contato.php
@@ -6,6 +6,11 @@ include __DIR__ . '/header.php';
 <!-- Conteúdo principal do site -->
 <main>
     <h1>Fale comigo</h1>
+    <div>
+        <a href="#" class="efeito-zoom">
+            <img src="assets/images/logo.webp" alt="">
+        </a>
+    </div>
     <p>Preencha o formulário</p>
 </main>
 
